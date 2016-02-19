2dba4c96b89299373e99d5b4ac8966a48a5abb7b
Vojnovič Milan <vojnovic@avast.com>
2016-02-19 11:25:38 +0100

    human readable representation of the objects Question and Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,14 +1,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
+@python_2_unicode_compatible
 class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
