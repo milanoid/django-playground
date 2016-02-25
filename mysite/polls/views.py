4edbff810008345a6a07650a3bd9aeb2f223a486
Vojnovič Milan <vojnovic@avast.com>
2016-02-25 13:56:18 +0100

    index /polls/ view using template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    response = HttpResponse("Hello, world. You're at the polls index.")
-    response.set_cookie("x-server-name", "django-dev-webserver")
-    return response
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
