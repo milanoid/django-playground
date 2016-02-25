6f4c837ed9c2c3cae859295726119a4fa29d2652
Vojnovič Milan <vojnovic@avast.com>
2016-02-25 13:39:22 +0100

    detail, result, vote basic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,16 @@ def index(request):
     response = HttpResponse("Hello, world. You're at the polls index.")
     response.set_cookie("x-server-name", "django-dev-webserver")
     return response
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
